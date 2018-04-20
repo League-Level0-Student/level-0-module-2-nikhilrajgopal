e8c6fa23b1b4683748a85538a4ebfdd8b91649e4
nikhilrajgopal <34866833+nikhilrajgopal@users.noreply.github.com>
2018-04-19 20:00:30 -0700

    Validation

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -20,10 +20,30 @@ public class Validation {
 		
 		System.out.println(randomNumber);
 
-		// 3. Use each value of randomNumber to give the user a random compliment.
-
+		// 3. Use each value of randomNumber to give the user a random compliment
+	for (int i = 0; i < 10; i++) {
+		
+		
+		if (randomNumber ==0) { 
+			JOptionPane.showMessageDialog(null, "I like your hair");
+		}
+		if (randomNumber ==1) {
+			JOptionPane.showMessageDialog(null, "I like your shoes");
+		}
+		if (randomNumber ==2) {
+			JOptionPane.showMessageDialog(null, "I like your shirt");
+		}
+		if (randomNumber ==3) {
+		    JOptionPane.showMessageDialog(null, "I like your backpack");
+		}
+		if (randomNumber ==4) {
+			JOptionPane.showMessageDialog(null, "I like your code");
+		}
+	}
+		
+		}
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
 	}
-}
+
