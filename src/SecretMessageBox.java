788f92bf8fbb0b0103a8a2aab3dd53a7a283e70d
league <league@imac-17.attlocal.net>
2019-07-24 16:32:55 -0700

    Secret Message Box, Unbirthday, The Riddler

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -5,5 +5,12 @@ public static void main(String[] args) {
 	String password="12345";
 	String user=JOptionPane.showInputDialog("Tell me a secret.");
 	System.out.println(user);
+	String nextuser=JOptionPane.showInputDialog("You can only see the secret if you guess the correct password.");
+	if (nextuser.equals(password)) {
+		JOptionPane.showMessageDialog(null, user);
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "INTRUDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	}
 }
 }
